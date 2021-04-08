c9bf8dae65d7cbbc25c8589b08dcf73d4a19bda0
HieuNguyen <hthnguyen@myseneca.ca>
2021-04-08 17:30:02 -0400

    Developed a new feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,18 @@
+/*********************************************************************************
+*  WEB422 – Assignment 06
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
+* 
+*  Name: Hoang Trung Hieu Nguyen     Student ID: 142914191     Date: 2021-04-08
+*
+*  Online Link to Music App: https://hthnguyen-spotify-api.netlify.app/
+*
+*  Online Link to User Api: https://api-seneca-music.herokuapp.com/api/user
+*
+********************************************************************************/ 
+
+
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { NavigationStart, Router , Event} from '@angular/router';
